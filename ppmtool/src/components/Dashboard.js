7691ba63edd3c9f3a7e79334def075f9c28c219c
Geca13 <karafiloski84@gmail.com>
2021-05-31 14:54:15 +0200

    finished the first section of the react

--- ppmtool/src/components/Dashboard.js
+++ ppmtool/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ProjectItem from './Project/ProjectItem';
-
+import CreateProjectButton from './Project/CreateProjectButton';
 class Dashboard extends Component {
     render() {
         return (
@@ -11,9 +11,7 @@ class Dashboard extends Component {
                 <div className="col-md-12">
                     <h1 className="display-4 text-center">Projects</h1>
                     <br />
-                    <a href="ProjectForm.html" class="btn btn-lg btn-info">
-                        Create a Project
-                    </a>
+                    <CreateProjectButton/>
                     <br />
                     <hr />
 
